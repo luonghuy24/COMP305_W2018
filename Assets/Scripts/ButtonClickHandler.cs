ae84a47cd8ff3fe2dda37e5760ce43a814cd3c6b
luonghuy24 <mrluonghuy@gmail.com>
2018-02-05 06:45:58 -0500

    lab02

---
+++ Assets/Scripts/ButtonClickHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClickHandler : MonoBehaviour {
+
+	public GameObject huy;
+	public List<Sprite> newImage;
+	private SpriteRenderer sRender;
+
+	// Use this for initialization
+	void Start () {
+		sRender = huy.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown(){		
+		switch (this.name) {
+			case "blue-btn":
+				sRender.sprite = newImage[0];
+				break;
+			case "red-btn":
+				sRender.sprite = newImage[1];
+				break;
+			case "yellow-btn":
+				sRender.sprite = newImage[2];
+				break;
+		}
+	}
+}
---
+++ Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHandler : MonoBehaviour {
+
+	public GameObject simple_humanoid;
+	public int someNumber;
+	public List<Sprite> newImage;
+
+	private Rigidbody2D rBody;
+	private SpriteRenderer sRender;
+
+	void Start () {
+		rBody = simple_humanoid.GetComponent<Rigidbody2D>();
+		rBody.velocity = new Vector2 (5, 0);
+		sRender = simple_humanoid.GetComponent<SpriteRenderer> ();
+	}
+
+	void Update () {
+		
+	}
+
+	// Guarenteed to run at a specific interval
+	// Physic (i.e.
+	void FixedUpdate(){
+	}		
+
+	void OnMouseDown(){
+		GameObject gObject = GameObject.Find ("Simple_humanoid");
+		sRender.sprite = newImage[0];
+		rBody.velocity *= -1;
+		Debug.Log (someNumber);
+	}
+
+}
